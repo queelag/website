3f3c2d804ac11336c343d333f1cb1d0f2e7f47dc
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-08-19 20:35:17 +0200

    Upgrade deps

--- src/definitions/stack-blitz/web/components/data.ts
+++ src/definitions/stack-blitz/web/components/data.ts
@@ -3,28 +3,13 @@ import type { ProjectFiles } from '@stackblitz/sdk';
 
 export const SB_AVATAR: ProjectFiles = {
   'index.html': html`
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-        <link rel="stylesheet" href="./index.css" />
-      </head>
-      <body>
-        <div id="root">
-          <aracna-avatar background="lightgray" shape="squircle" size="32">
-            D
-          </aracna-avatar>
-        </div>
-        <script type="module" src="./index.js"></script>
-      </body>
-    </html>
+    <aracna-avatar background="lightgray" shape="squircle" size="32">
+      D
+    </aracna-avatar>
   `,
   'index.js': html`
     <script>
-      import '@aracna/web-components/elements/data/avatar-element.js';
+      import '@aracna/web-components/index.iife.js';
     </script>
   `
 };
